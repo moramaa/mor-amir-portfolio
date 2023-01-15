fead93059d20905ab4e14a065cc1259c4590d890
Mor Amir <imoramira@gmail.com>
2023-01-15 12:57:42 +0200

    33

---
+++ sanity/schemas/schema.js
@@ -0,0 +1,11 @@
+import createSchema from "part:@sanity/base/schema-creator";
+import experience from "./experience";
+import skill from "./skill";
+import pageInfo from "./pageInfo";
+import social from "./social";
+import project from "./project";
+
+export default createSchema({
+name: "default",
+types: schemaTypes.concat([skill, pageInfo, social,project, experience]),
+});
