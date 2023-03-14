fe1de398b0a66e9fab191db12187fb4427ebe281
Mor Amir <imoramira@gmail.com>
2023-03-14 15:23:20 +0200

    fix tracking

--- components/Main.tsx
+++ components/Main.tsx
@@ -12,8 +12,8 @@ const Main = ({pageInfo}: Props) => {
   return (
     <div 
     className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-4xl text-center'>{pageInfo.name}</h3>
-        <h4 className='absolute top-24 mt-10 uppercase tracking-[10px] text-gray-500 text-3xl text-center'>{pageInfo.role} </h4>
+        <h3 className=' absolute top-20 uppercase tracking-[2px] text-gray-500 text-3xl text-center'>{pageInfo.name}</h3>
+        <h4 className='absolute top-24 mt-10 uppercase tracking-[8px] text-gray-500 text-2xl text-center'>{pageInfo.role} </h4>
        
         <motion.div
         className='mt-40 md:mt-10'  
